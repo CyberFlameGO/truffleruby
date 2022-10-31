e3aa16b223667a17eedd542f20b6753c6deee02b
Benoit Daloze <benoit.daloze@oracle.com>
2022-10-31 13:41:24 +0100

    Improve tool/local-backtrace.rb

--- tool/local-backtrace.rb
+++ tool/local-backtrace.rb
@@ -2,5 +2,6 @@
 root = File.dirname(__dir__)
 contents = STDIN.read
 contents = contents.gsub("/b/b/e/main", ".")
+contents = contents.gsub(%r{/Users/graal\d+/slave/e/main}, ".")
 contents = contents.gsub("/mxbuild/truffleruby-jvm/jre/languages/ruby/", "/")
 STDOUT.puts contents
